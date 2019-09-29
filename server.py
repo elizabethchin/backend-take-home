9a21823cbb9ef5b049874d8a4d5ee64e1c0a10ed
Elizabeth Chin <elizabethtchin@gmail.com>
2019-09-28 23:18:28 -0700

    add local host

--- server.py
+++ server.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = False
+    connect_to_db(app)
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
